a1a5dac5d0ac5138e7dd3d418c9448e67205a092
Phil <phil.estival@free.fr>
2017-03-24 12:56:46 +0100

    fix package license

--- pyorgmode/__init__.py
+++ pyorgmode/__init__.py
@@ -4,7 +4,7 @@ from .PyOrgMode import (OrgNode, OrgElement, OrgDataStructure, OrgDate)
 
 __title__ = 'pyorgmode'
 __version__ = '0.2.0a'
-__authors__ = ''
-__license__ = ''
-__copyright__ = ''
+__authors__ = 'the pyorgmode team'
+__license__ = 'GPLv3'
+__copyright__ = 'Jonathan BISSON'
 __all__ = ['OrgNode','OrgElement', 'OrgDataStructure', 'OrgDate']
